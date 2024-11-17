23ae2d2f92b73cfa5903350bc95ce7acd9c4de06
Francisca Pereira <franciscapereira@student.dei.uc.pt>
2024-11-17 15:41:18 +0000

    create Ball class2

---
+++ src/Ball.cpp
@@ -0,0 +1,7 @@
+// Ballcpp - Main file for the Ball class
+
+// Author: Francisca Pereira
+// Student ID: 2022213550
+// Version: 1.0
+
+#include "Ball.h"
---
+++ src/Ball.h
@@ -0,0 +1,18 @@
+// Ball.h - Header file for the Ball class
+
+// Author: Francisca Pereira
+// Student ID: 2022213550
+// Version: 1.0
+
+#ifndef BALL_H
+#define BALL_H
+
+#include "ofMain.h"
+
+class Ball {
+public:
+	Ball();
+};
+
+#endif // BALL_H
+
